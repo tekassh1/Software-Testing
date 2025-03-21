a36da8f477dc457478b53f8e3c057c409c7d02f7
geeks <geekslovee@gmail.com>
2025-03-21 18:37:51 +0300

    fixed.

--- Lab_1/src/test/java/TestPart3.java
+++ Lab_1/src/test/java/TestPart3.java
@@ -4,7 +4,6 @@ import org.example.Part3.Enums.Location;
 import org.example.Part3.Enums.Place;
 import org.example.Part3.LocationArea;
 import org.example.Part3.TwoHeadedMan;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
